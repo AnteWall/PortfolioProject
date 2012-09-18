d3bc14d2f5a4ea23312c68c4e9d4d8b276cee243
Ante <antewall@gmail.com>
2012-09-18 15:45:59 +0200

    Change folder structure and started on Flask/Jinja2 HTML coding

--- Portfolio/web/testa.py
+++ Portfolio/web/testa.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 from flask import Flask,render_template
-#from jinja2 import Templates
+from jinja2 import Template
 
 app = Flask(__name__)
 
-@app.route("/static/<filename>")
+@app.route("/web/style/<filename>")
 def css(filename):
-    with app.open_resource("static/" +filename) as f:
+    with app.open_resource("web/style/" +filename) as f:
         return f.read()
-@app.route("/images/<filename>")
+@app.route("/web/images/<filename>")
 def images(filename):
-    with app.open_resource("images/" + filename) as f:
+    with app.open_resource("web/images/" + filename) as f:
         return f.read()
 
 @app.route("/")
 def home_page():
     return render_template("home.html")
-
 if __name__ == "__main__":
     app.debug = True
     app.run()
